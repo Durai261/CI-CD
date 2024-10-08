d846231f0ea15393a3d2e01883d48872d78d9873
Durai261 <durai2698@gmail.com>
2024-10-08 07:40:09 +0530

    Update start_container.sh

--- aws-projects/start_container.sh
+++ aws-projects/start_container.sh
@@ -6,5 +6,5 @@ echo
 docker push durai261/simple-python-flask-app1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 durai261/simple-python-flask-app1
+docker run -d -p 8000:8000 durai261/simple-python-flask-app1
 echo
